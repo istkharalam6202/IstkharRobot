f2fa30ed6b8499af83448284abe19f3aea32fe37
istkharalam6202 <istkharalam6202@gmail.com>
2025-01-11 06:18:29 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -79,10 +79,11 @@ PM_START_TEXT = """
 **❖ ᴛʜɪs ɪs {} 🎶 !** \n\n**━━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━━**\n**❖ ᴛʜɪs ɪs ϻᴧηᴧɢєϻєηᴛ | ϻυsɪᴄ ʙσᴛ**\n**❖ ησ ʟᴧɢ | ᴧᴅs ϻυsɪᴄ | ησ ᴘʀσϻσ**\n**❖ 24x7 ʀυη | ʙєsᴛ sσυηᴅ ǫυᴧʟɪᴛʏ**\n**━━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━━**\n**❖ ᴄʟɪᴄᴋ ση ᴛʜє ʜєʟᴩ ʙυᴛᴛση ᴛσ ɢєᴛ ɪηғσ**\n**    ᴧʙσυᴛ ϻʏ ϻσᴅυʟєs ᴧηᴅ ᴄσϻϻᴧηᴅs...!**\n**━━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━━**"""
 
 buttons = [
-    [
+        [
         
-        InlineKeyboardButton(text="▪️ʜᴏᴍᴇ▪️", callback_data="mukesh_"),
-   InlineKeyboardButton(text="▪️ᴏᴡɴᴇʀ▪️", url=f"tg://user?id={OWNER_ID}"),
+        InlineKeyboardButton(text="▪️▪️", callback_data="mukesh_"),
+      InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="source_"),
+       InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f"tg://user?id={OWNER_ID}"),
         InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅ", callback_data="Main_help"),
      ],
         InlineKeyboardButton(
